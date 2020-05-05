d13389525c827b78f97f38ebfd18762bbc0d25ab
andipurnama <andipurnama2112@gmail.com>
2020-05-05 19:53:41 +0700

    v-01

---
+++ app/src/main/java/com/example/uts2/DocDetail.java
@@ -0,0 +1,14 @@
+package com.example.uts2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_detail);
+    }
+}
